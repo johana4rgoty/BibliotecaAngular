508d0a00826dd1aceca98196426e9e667bae0119
estephany2828 <ejargoty@unicauca.edu.co>
2019-06-17 10:43:43 -0500

    frontend de app.component.html completo con su css falta login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap';
+// import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MatButtonModule,
+    // MatCheckboxModule,
+    // BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
